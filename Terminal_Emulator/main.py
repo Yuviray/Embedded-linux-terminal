a449d7c15e944a931775574d9f935453d346a7fd
Yuviray <110566660+Yuviray@users.noreply.github.com>
2023-04-02 17:46:34 -0500

    Update main.py

--- Terminal_Emulator/main.py
+++ Terminal_Emulator/main.py
@@ -1,35 +1,44 @@
 import os
 
-path = os.getcwd()
-
 def ls():
-    files = os.listdir(path)
+    files = os.listdir(os.getcwd())
     for file in files:
         print(file)
 
-def cd():
-    path = input("path : ")
+def cd(path):
     os.chdir(path)
 
-def mkfile():
-    file = input("File name : ")
+def mkfile(file):
+
     file_name = open(file,'w')
     file_name.close()
-def mkfolder():
-    folder = input("folder name : ")
+def mkfolder(folder):
     os.mkdir(folder)
 
 def main():
     while True:
-        command = input("[terminal]$ ")
-        if command == "ls":
+        term_input = input(os.getcwd() + "$ ")
+
+        # Split the input into command and arguments
+        command = term_input.split()
+        if command[0] == "ls":
             ls()
-        if command == "cd":
-            cd()
-        if command == "cat":
-            mkfile()
-        if command == "mkdir":
-            mkfolder()
+        if command[0] == "cd":
+            if len(command) > 1:
+                cd(command[1])
+            else:
+                print("Error: Please specify a path.")
+        if command[0] == "cat":
+            if len(command) > 1:
+                mkfile(command[2])
+            else:
+                print("Error: Please specify a path.")
+
+        if command[0] == "mkdir":
+            if len(command) > 1:
+                mkfolder(command[1])
+            else:
+                print("Error: Please specify a path.")
 
 if __name__ == "__main__":
     main()
