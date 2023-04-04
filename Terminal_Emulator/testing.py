498b124d3818479477ace180045bd29e15f049b8
Collan21 <Collan21@users.noreply.github.com>
2023-04-04 12:05:43 -0500

    implemented 'cd', fixed input and output drawing

--- Terminal_Emulator/testing.py
+++ Terminal_Emulator/testing.py
@@ -1,19 +1,36 @@
 import pygame
+import os
+
+def cd(path):
+    current_path = getPathText()
+    
+    try:
+        os.chdir(current_path[:-2] + "\\" + path)
+    except:
+        print("Error: Please enter a valid path.")
+
 
 def getPathText():
-    pathText = "PS C:\\Users\\user>"
-    return pathText
+    current_folder = os.getcwd()
+    return current_folder + "> "
 
-def setCommand(userInput):
-    print(userInput)
+def callCommand(user_input):
+    command = user_input.split()
+
+    if command[1] == "cd":
+        if len(command) > 2:
+            cd(command[2])
+
+        else:
+            print("Error: Please specify a path.")
 
 def main():
     # Initialize Pygame
     pygame.init()
 
     # Set the screen size
-    screen_width = 800
-    screen_height = 400
+    screen_width = 1000
+    screen_height = 500
     screen = pygame.display.set_mode((screen_width, screen_height))
 
     # Define terminal colors
@@ -22,20 +39,26 @@ def main():
     cursor_color = (255, 255, 255)
 
     # Define font
-    font_size = 24
+    font_size = 16
     font = pygame.font.Font('fonts/UbuntuMono-Regular.ttf', font_size)
 
     # Define text input box
-    input_box = pygame.Rect(0, 0, screen_width - 20, font_size * 2)
-    text = ""
+    input_box = pygame.Rect(0, 0, screen_width, screen_height)
     pathText = getPathText()
-    active = False
+    text = pathText
+    active = True # can set false if user should click before typing
+    next_y = 0
 
     # Define cursor variables
     cursor_surface = font.render("|", True, cursor_color)
     cursor_visible = True
     cursor_blink_time = 500  # Time between cursor blinks in milliseconds
     cursor_blink_timer = 0
+
+    # Define command history
+    command_history = []
+    max_history_length = 50
+    history_index = 0
         
     # Game loop
     while True:
@@ -51,37 +74,69 @@ def main():
                     active = True
                 else:
                     active = False
+
             elif event.type == pygame.KEYDOWN:
-                # Check if the user typed a key
+                # Check if the user typed a key and clicked on the window
                 if active:
+
                     if event.key == pygame.K_RETURN:
                         # Process the user's input
-                        setCommand(text)
-                        text = ""
+                        callCommand(text)
+
+                        # Update command history
+                        command_history.append(text)
+                        if len(command_history) > max_history_length:
+                            command_history.pop(0)
+                        history_index = len(command_history)
+
+                        # update previous command output
+                        output_surface =  font.render(text, True, text_color)
+                        screen.blit(output_surface, (10, next_y))
+                        text = getPathText()
+                        next_y += font.get_height() + 8
+
                     elif event.key == pygame.K_BACKSPACE:
                         text = text[:-1]
+
+                    elif event.key == pygame.K_UP:
+                        if history_index > 0:
+                            history_index -= 1
+                            text = command_history[history_index]
+
+                    elif event.key == pygame.K_DOWN:
+                        if history_index < len(command_history) - 1:
+                            history_index += 1
+                            text = command_history[history_index]
+
                     else:
-                        text += event.unicode    
+                        text += event.unicode  
+
+        # Update the path text
+        pathText = getPathText()
 
         # Draw the terminal background
         screen.fill(background_color)
 
+        # Draw the previous commands
+        for i in range(len(command_history)):
+            command_surface = font.render(command_history[i], True, text_color)
+            screen.blit(command_surface, (10, i * (font.get_height() + 8)))
+        
         # Draw the text
-        text_surface = font.render(pathText + " " + text, True, text_color)
-        screen.blit(text_surface, (0, 10))
+        text_surface = font.render(text, True, text_color)
+        screen.blit(text_surface, (10, next_y))
 
-            # Draw the cursor
+        # Draw the cursor
         if active:
             if pygame.time.get_ticks() - cursor_blink_timer > cursor_blink_time:
                 cursor_blink_timer = pygame.time.get_ticks()
                 cursor_visible = not cursor_visible
 
             if cursor_visible:
-                cursor_pos = text_surface.get_width()
-                screen.blit(cursor_surface, (cursor_pos, 10))
+                cursor_pos = text_surface.get_width() + 8
+                screen.blit(cursor_surface, (cursor_pos, next_y))
 
         # Update the display
         pygame.display.update()
-
 
 main()
