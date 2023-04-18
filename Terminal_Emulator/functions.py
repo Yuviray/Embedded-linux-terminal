032e33d44242a67459d759651b8b7d1a360c9dca
Yuvanesh Rajamani <jjd149@txstate.edu>
2023-04-18 13:34:20 -0500

    added more functions and unit tests

--- Terminal_Emulator/functions.py
+++ Terminal_Emulator/functions.py
@@ -33,6 +33,53 @@ def cp(src, dst):
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
     shutil.copy(src, dst)
+    
+def touch(file_name):
+    new_file = open(file_name, 'w')
+    new_file.close()
+
+def chmod(permission, file_name):
+    os.chmod(file_name, permission)
+
+def chown(owner, file_name):
+    os.chown(file_name, owner, -1)
+
+def grep(pattern, file_name):
+    with open(file_name, 'r') as file:
+        matching_lines = [line for line in file if pattern in line]
+    return matching_lines
+#def ln(src, dst):
+#    os.symlink(src, dst)
+
+#def du(file_name):
+#    cmd = 'du -sh {}'.format(file_name)
+#    output = os.popen(cmd).read()
+#    return output
+
+def df():
+    cmd = 'df'
+    output = os.popen(cmd).read()
+    return output
+
+#def find(path, option):
+#    cmd = 'find {} {}'.format(path, option)
+#    output = os.popen(cmd).read()
+#    return output
+
+def head(file_name,n):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        return lines[:n]
+
+def tail(file_name, n):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        return lines[-n:]
+
+def wget(url):
+    cmd = 'wget {}'.format(url)
+    output = os.popen(cmd).read()
+    return output
 
 
 def getPathText():
