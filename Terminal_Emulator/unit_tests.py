442172a15547ce7a6e72921470a69efbb6089cf2
Yuvanesh Rajamani <jjd149@txstate.edu>
2023-04-25 00:51:07 -0500

    got unit test working with classes

--- Terminal_Emulator/unit_tests.py
+++ Terminal_Emulator/unit_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch
 from game import*
+from functions import FileManagement
 from functions import*
 import shutil
 from unittest.mock import patch
@@ -8,28 +9,29 @@ from io import StringIO
 
 
 class TestCommandLine(unittest.TestCase):
+    file_manager = FileManagement()
     def test_cd_command_with_valid_path(self):
         with patch('os.chdir') as mock_chdir:
-            cd("test_dir")
+            file_manager.cd("test_dir")
             mock_chdir.assert_called_once_with('/Users/yuvaneshrajamani/PycharmProjects/Embedded-linux-terminal/Terminal_Emulator/test_dir') # replace with expected path
     
     def test_ls_command(self):
         with open("test.txt", "w") as f:
             f.write("test")
-        self.assertIn("test.txt", ls())
+        self.assertIn("test.txt", file_manager.ls())
         os.remove("test.txt")
         
     def test_cat_command(self):
         file_name = "test.txt"
         with open(file_name, "w") as f:
             f.write("test")
-        cat(file_name)
+        file_manager.cat(file_name)
         self.assertTrue(os.path.exists(file_name))
         os.remove(file_name)
         
     def test_mkdir_command(self):
         dir_name = "test_dir"
-        mkdir(dir_name)
+        file_manager.mkdir(dir_name)
         self.assertTrue(os.path.exists(dir_name))
         os.rmdir(dir_name)
         
@@ -38,20 +40,20 @@ class TestCommandLine(unittest.TestCase):
         test_file = 'test_file.txt'
         with open(test_file, 'w') as f:
             f.write('test')
-        rm(test_file)
+        file_manager.rm(test_file)
         self.assertFalse(os.path.exists(test_file))
 
     def test_rmdir(self):
         # Create a test directory and remove it
         test_dir = 'test_dir'
         os.mkdir(test_dir)
-        rmdir(test_dir)
+        file_manager.rmdir(test_dir)
         self.assertFalse(os.path.exists(test_dir))
 
     def test_pwd(self):
         # Test that pwd() returns the current working directory
         current_dir = os.getcwd()
-        self.assertEqual(pwd(), current_dir)
+        self.assertEqual(file_manager.pwd(), current_dir)
 
     def test_mv(self):
         # Create a test file and move it to a new location
@@ -63,7 +65,7 @@ class TestCommandLine(unittest.TestCase):
             shutil.rmtree(new_dir)
         os.mkdir(new_dir)
         new_location = os.path.join(new_dir, test_file)
-        mv(test_file, new_location)
+        file_manager.mv(test_file, new_location)
         self.assertTrue(os.path.exists(new_location))
         self.assertFalse(os.path.exists(test_file))
         shutil.rmtree(new_dir)
@@ -78,50 +80,50 @@ class TestCommandLine(unittest.TestCase):
             shutil.rmtree(new_dir)
         os.mkdir(new_dir)
         new_location = os.path.join(new_dir, test_file)
-        cp(test_file, new_location)
+        file_manager.cp(test_file, new_location)
         self.assertTrue(os.path.exists(new_location))
         self.assertTrue(os.path.exists(test_file))
         shutil.rmtree(new_dir)
         
     def test_getPathText(self):
-        self.assertIsInstance(getPathText(), str)
+        self.assertIsInstance(file_manager.getPathText(), str)
         
     def test_touch(self):
         file_name = 'test_file.txt'
-        touch(file_name)
+        file_manager.touch(file_name)
         self.assertTrue(os.path.exists(file_name))
         os.remove(file_name)
         
     def test_chmod(self):
         file_name = 'test_file.txt'
-        touch(file_name)
+        file_manager.touch(file_name)
         with open(file_name, 'w') as file:
             file.write('test data')
-        chmod(0o644, file_name)
+        file_manager.chmod(0o644, file_name)
         self.assertEqual(os.stat(file_name).st_mode & 0o777, 0o644)
         os.remove(file_name)
         
     def test_chown(self):
         file_name = 'test_file.txt'
-        touch(file_name)
+        file_manager.touch(file_name)
         with open(file_name, 'w') as file:
             file.write('test data')
-        chown(1000, file_name)
+        file_manager.chown(1000, file_name)
         self.assertEqual(os.stat(file_name).st_uid, 1000)
         os.remove(file_name)
         
     def test_grep(self):
         file_name = 'test_file.txt'
-        touch(file_name)
+        file_manager.touch(file_name)
         with open(file_name, 'w') as file:
             file.write('line1\nline2\nline3\nline4\nline5')
-        matching_lines = grep('line', file_name)
+        matching_lines = file_manager.grep('line', file_name)
         self.assertEqual(matching_lines, ['line1\n', 'line2\n', 'line3\n', 'line4\n', 'line5'])
         os.remove(file_name)
         
     @patch('sys.stdout', new_callable=StringIO)
     def test_df(self, mock_stdout):
-        df()
+        file_manager.df()
         self.assertIn('Filesystem', mock_stdout.getvalue())
         self.assertIn('1K-blocks', mock_stdout.getvalue())
         self.assertIn('Used', mock_stdout.getvalue())
