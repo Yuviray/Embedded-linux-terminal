ad90916b8fb87ff25d4821a8b0e3af569fb7343b
Yuvanesh Rajamani <jjd149@txstate.edu>
2023-04-26 00:10:04 -0500

    added more commmands and added unittests for them
    all tests pass and fixed bug with unittest

--- Terminal_Emulator/unit_tests.py
+++ Terminal_Emulator/unit_tests.py
@@ -6,7 +6,9 @@ from functions import*
 import shutil
 from unittest.mock import patch
 from io import StringIO
-
+import socket
+import platform
+from datetime import datetime
 
 class TestCommandLine(unittest.TestCase):
     file_manager = FileManagement()
@@ -36,7 +38,6 @@ class TestCommandLine(unittest.TestCase):
         os.rmdir(dir_name)
         
     def test_rm(self):
-        # Create a test file and remove it
         test_file = 'test_file.txt'
         with open(test_file, 'w') as f:
             f.write('test')
@@ -44,19 +45,16 @@ class TestCommandLine(unittest.TestCase):
         self.assertFalse(os.path.exists(test_file))
 
     def test_rmdir(self):
-        # Create a test directory and remove it
         test_dir = 'test_dir'
         os.mkdir(test_dir)
         file_manager.rmdir(test_dir)
         self.assertFalse(os.path.exists(test_dir))
 
     def test_pwd(self):
-        # Test that pwd() returns the current working directory
         current_dir = os.getcwd()
         self.assertEqual(file_manager.pwd(), current_dir)
 
     def test_mv(self):
-        # Create a test file and move it to a new location
         test_file = 'test_file.txt'
         with open(test_file, 'w') as f:
             f.write('test')
@@ -71,7 +69,6 @@ class TestCommandLine(unittest.TestCase):
         shutil.rmtree(new_dir)
 
     def test_cp(self):
-        # Create a test file and copy it to a new location
         test_file = 'test_file.txt'
         with open(test_file, 'w') as f:
             f.write('test')
@@ -86,7 +83,7 @@ class TestCommandLine(unittest.TestCase):
         shutil.rmtree(new_dir)
         
     def test_getPathText(self):
-        self.assertIsInstance(file_manager.getPathText(), str)
+        self.assertIsInstance(file_manager.get_path_text(), str)
         
     def test_touch(self):
         file_name = 'test_file.txt'
@@ -102,16 +99,7 @@ class TestCommandLine(unittest.TestCase):
         file_manager.chmod(0o644, file_name)
         self.assertEqual(os.stat(file_name).st_mode & 0o777, 0o644)
         os.remove(file_name)
-        
-    def test_chown(self):
-        file_name = 'test_file.txt'
-        file_manager.touch(file_name)
-        with open(file_name, 'w') as file:
-            file.write('test data')
-        file_manager.chown(1000, file_name)
-        self.assertEqual(os.stat(file_name).st_uid, 1000)
-        os.remove(file_name)
-        
+
     def test_grep(self):
         file_name = 'test_file.txt'
         file_manager.touch(file_name)
@@ -128,6 +116,103 @@ class TestCommandLine(unittest.TestCase):
         self.assertIn('1K-blocks', mock_stdout.getvalue())
         self.assertIn('Used', mock_stdout.getvalue())
         self.assertIn('Available', mock_stdout.getvalue())
+        
+    def test_tail(self):
+        with open("test_tail.txt", "w") as f:
+            f.write("Line 1\nLine 2\nLine 3\nLine 4\nLine 5")
+        result = self.file_manager.tail("test_tail.txt", 3)
+        self.assertEqual(result, ["Line 3\n", "Line 4\n", "Line 5"])
+        os.remove("test_tail.txt")
+
+    def test_date(self):
+        current_date = self.file_manager.date()
+        now = datetime.now().strftime("%a %b %d %H:%M:%S %Y")
+        self.assertEqual(current_date, now)
+
+    def test_uname(self):
+        uname_result = self.file_manager.uname()
+        self.assertEqual(uname_result, platform.uname())
+
+    def test_hostname(self):
+        hostname_result = self.file_manager.hostname()
+        self.assertEqual(hostname_result, socket.gethostname())
+
+    def test_whoami(self):
+        whoami_result = self.file_manager.whoami()
+        self.assertEqual(whoami_result, os.getlogin())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_echo(self, mock_stdout):
+        self.file_manager.echo("hello", "world", sep="-", end="!")
+        self.assertEqual(mock_stdout.getvalue(), "hello-world!")
+    
+    @patch('os.popen')
+    def test_wget(self, mock_popen):
+        self.file_manager.wget('https://example.com')
+        mock_popen.assert_called_once_with('wget https://example.com')
+    
+    @patch('shutil.disk_usage')
+    def test_df(self, mock_disk_usage):
+        mock_disk_usage.return_value = (1024, 512, 256)
+        with patch('builtins.print') as mock_print:
+            self.file_manager.df()
+            mock_print.assert_any_call("Filesystem      1K-blocks    Used      Available")
+            mock_print.assert_any_call("/               1      0      0")
+
+    def test_find(self):
+        test_dir = "test_dir"
+        os.makedirs(os.path.join(test_dir, "subdir"))
+        with open(os.path.join(test_dir, "file1.txt"), "w") as f:
+            f.write("test")
+        with open(os.path.join(test_dir, "subdir", "file2.txt"), "w") as f:
+            f.write("test")
+        result = self.file_manager.find(test_dir, name="file1.txt")
+        self.assertEqual(len(result), 1)
+        self.assertIn(os.path.join(test_dir, "file1.txt"), result)
+        result = self.file_manager.find(test_dir, name="file2.txt", type='f')
+        self.assertEqual(len(result), 1)
+        self.assertIn(os.path.join(test_dir, "subdir", "file2.txt"), result)
+        shutil.rmtree(test_dir)
+    
+    def test_touch(self):
+        test_file = "test_file.txt"
+        self.file_manager.touch(test_file)
+        self.assertTrue(os.path.exists(test_file))
+        os.remove(test_file)
+
+    def test_head(self):
+        test_file = "test_file.txt"
+        with open(test_file, 'w') as file:
+            file.write("line1\nline2\nline3\nline4\nline5")
+        
+        result = self.file_manager.head(test_file, 3)
+        self.assertEqual(result, ["line1\n", "line2\n", "line3\n"])
+        os.remove(test_file)
+
+    @patch('builtins.print')
+    def test_echo(self, mock_print):
+        self.file_manager.echo("hello", "world", sep=", ")
+        mock_print.assert_called_once_with("hello, world", end='\n')
+
+    @patch('subprocess.run')
+    def test_ping(self, mock_subprocess_run):
+        self.file_manager.ping("localhost")
+        mock_subprocess_run.assert_called_once()
+
+    @patch('subprocess.run')
+    def test_ps(self, mock_subprocess_run):
+        self.file_manager.ps()
+        mock_subprocess_run.assert_called_once()
+
+    @patch('psutil.process_iter')
+    def test_top(self, mock_process_iter):
+        self.file_manager.top()
+        mock_process_iter.assert_called_once()
+
+    @patch('psutil.net_if_addrs')
+    def test_ifconfig(self, mock_net_if_addrs):
+        self.file_manager.ifconfig()
+        mock_net_if_addrs.assert_called_once()
 
     
 if __name__ == '__main__':
